b419b7f77207474efa02c68fcfb3d9604c3e88c2
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-02-03 14:20:49 +0900

    feat - 엔티티 클래스

---
+++ src/main/java/jpa/study/domain/Delivery.java
@@ -0,0 +1,21 @@
+package jpa.study.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.tomcat.jni.Address;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+    @Embedded
+    private Address address;
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; //ENUM [READY(준비), COMP(배송)]
+}
