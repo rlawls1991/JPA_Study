42cd2f6e28846b00c2be15568e1b998dedfc4e18
rlawls1991 <rlawls1991@gmail.com>
2021-02-03 22:35:32 +0900

    feat - 설명추가

--- src/main/java/jpa/study/domain/Order.java
+++ src/main/java/jpa/study/domain/Order.java
@@ -18,19 +18,45 @@ public class Order {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member; //주문 회원
+
+    /*
+    cascade = CascadeType.ALL는
+    orderItem이 여러개 저장이되면 아래처럼 행위를 해야되는데
+    아래처럼 할 필요 없이 한꺼번에 해주는 기능을 가짐
+    persist(orderItemA)
+    persist(orderItemB)
+    persist(orderItemC)
+    persist(order)
+
+    즉 persist(order) 이 행위만 해주면 됨
+     */
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
+
+
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery; //배송정보
     private LocalDateTime orderDate; //주문시간
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
-    //==연관관계 메서드==//
+
+    //==연관관계 메서드== 컨틀로하는쪽이 가지는게 좋다. //
+    /*
+    setMember 메소드 풀어쓰면 아래와 같다.
+    public static void main(){
+        Member member = new member;
+        Order ordr = new Order();
+
+        member.getOrders().add((order);
+        order.setMember(member);
+    }
+     */
     public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
     }
+
     public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
