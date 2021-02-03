a139eae0c22cdb16595c207d9a01801c43818e06
rlawls1991 <rlawls1991@gmail.com>
2021-02-03 22:35:47 +0900

    style - 코드 정리

--- src/main/java/jpa/study/domain/Address.java
+++ src/main/java/jpa/study/domain/Address.java
@@ -4,8 +4,10 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
     protected Address() {
     }
+
     public Address(String city, String street, String zipcode) {
         this.city = city;
         this.street = street;
