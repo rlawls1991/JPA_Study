2946043e4138d6df58e3e6ff83dd6352c403527c
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-02-04 14:19:10 +0900

    feat - 주문 서비스, 주문 리포지토리 개발

---
+++ src/main/java/jpa/study/service/OrderService.java
@@ -0,0 +1,60 @@
+package jpa.study.service;
+
+import jpa.study.domain.Delivery;
+import jpa.study.domain.Member;
+import jpa.study.domain.Order;
+import jpa.study.domain.OrderItem;
+import jpa.study.domain.item.Item;
+import jpa.study.repository.ItemRepository;
+import jpa.study.repository.MemberRepository;
+import jpa.study.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+
+    public Long order(Long memberId, Long itemId, int count) {
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.crateOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOne(orderId);
+        //주문 취소
+        order.cancel();
+    }
+
+    /**
+     * 검색
+     */
+    /*
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAll(orderSearch);
+    }
+     */
+}
